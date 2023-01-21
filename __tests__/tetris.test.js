258528da5a591d4ef30f7cdf6894929e67abe1a4
Michal Jurczuk <michal.jurczuk91@gmail.com>
2023-01-21 13:22:12 +0100

    rewritten tests to work with 4x4 pieces

--- __tests__/tetris.test.js
+++ __tests__/tetris.test.js
@@ -34,18 +34,18 @@ test("state update moves the piece when arrow keys are pressed", () => {
     expect(pieceStartingX).toBe((GAME_WIDTH/2) - 2);
 
     state = state.update(state, {"ArrowRight": true});
-    expect(state.piece.x).toBe(GAME_WIDTH / 2 + 1);
+    expect(state.piece.px).toBe(pieceStartingX + 1);
 
     state = state.update(state, {"ArrowLeft": true});
-    expect(state.piece.x).toBe(GAME_WIDTH / 2);
+    expect(state.piece.px).toBe(pieceStartingX);
 
     state = state.update(state, {"ArrowDown": true});
-    expect(state.piece.y).toBe(1);
-    expect(state.piece.x).toBe(GAME_WIDTH / 2);
+    expect(state.piece.py).toBe(pieceStartingY + 1);
+    expect(state.piece.px).toBe(pieceStartingX);
 });
 
 test("state removeRow function removes a finished row and shifts the grid down 1 square", () => {
-    let state = getStateWithOneLineFilled();
+    let state = getStateWithFilledLines([15]);
     state.grid[14][6] = true;
     state = state.removeRow(15, state);
 
